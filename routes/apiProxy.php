b9edf78b18fefa22c0195d09dae395a79ac19f71
Galactic1106 <aleksander.sardara@gmail.com>
2025-11-22 21:38:30 +0100

    Add Select exports, Ziggy and cards index route

--- routes/apiProxy.php
+++ routes/apiProxy.php
@@ -1,16 +1,16 @@
 <?php
 
-use App\Http\Controllers\YapImgRequestController;
-use App\Http\Controllers\YapRawRequestController;
-use App\Http\Controllers\YapRequestController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\YapArchetypesController;
 use App\Http\Controllers\YapAttributesController;
 use App\Http\Controllers\YapFormatsController;
 use App\Http\Controllers\YapFrameTypesController;
+use App\Http\Controllers\YapImgRequestController;
 use App\Http\Controllers\YapLinkMarkersController;
 use App\Http\Controllers\YapRacesController;
+use App\Http\Controllers\YapRawRequestController;
+use App\Http\Controllers\YapRequestController;
 use App\Http\Controllers\YapTypesController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/yap', YapRequestController::class);
 Route::get('/yap/raw?url={url}', YapRawRequestController::class);
