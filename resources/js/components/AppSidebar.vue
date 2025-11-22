c92cdaa17ea21afaa0905bafd3157693625c94d1
Galactic1106 <aleksander.sardara@gmail.com>
2025-11-22 21:40:45 +0100

    Add main navigation items to sidebar

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -2,24 +2,44 @@
 import NavFooter from '@/components/NavFooter.vue';
 import NavMain from '@/components/NavMain.vue';
 import NavUser from '@/components/NavUser.vue';
-import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link, usePage } from '@inertiajs/vue3';
+import { Home, LayoutGrid } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
-const mainNavItems: NavItem[] = [];
+const mainNavItems: NavItem[] = [
+  {
+    title: 'Home',
+    href: route('home'),
+    icon: Home,
+  },
+  {
+    title: 'Browse Cards',
+    href: route('cards.index'),
+    icon: LayoutGrid,
+  },
+];
 
 const footerNavItems: NavItem[] = [];
 
 const loginSignin: NavItem[] = [
-    {
-        title: 'Log in',
-        href: route('login'),
-    },
-    {
-        title: 'Sign in',
-        href: route('register'),
-    },
+  {
+    title: 'Log in',
+    href: route('login'),
+  },
+  {
+    title: 'Sign in',
+    href: route('register'),
+  },
 ];
 
 const page = usePage();
@@ -27,28 +47,28 @@ const user = page.props.auth.user;
 </script>
 
 <template>
-    <Sidebar collapsible="icon" variant="inset">
-        <SidebarHeader>
-            <SidebarMenu>
-                <SidebarMenuItem>
-                    <SidebarMenuButton size="lg" as-child>
-                        <Link :href="route('home')">
-                            <AppLogo />
-                        </Link>
-                    </SidebarMenuButton>
-                </SidebarMenuItem>
-            </SidebarMenu>
-        </SidebarHeader>
+  <Sidebar collapsible="icon" variant="inset">
+    <SidebarHeader>
+      <SidebarMenu>
+        <SidebarMenuItem>
+          <SidebarMenuButton size="lg" as-child>
+            <Link :href="route('home')">
+              <AppLogo />
+            </Link>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+      </SidebarMenu>
+    </SidebarHeader>
 
-        <SidebarContent>
-            <NavMain :items="mainNavItems" />
-        </SidebarContent>
+    <SidebarContent>
+      <NavMain :items="mainNavItems" />
+    </SidebarContent>
 
-        <SidebarFooter>
-            <NavFooter :items="footerNavItems" />
-            <NavUser v-if="user" />
-            <NavMain v-else :items="loginSignin"> </NavMain>
-        </SidebarFooter>
-    </Sidebar>
-    <slot />
+    <SidebarFooter>
+      <NavFooter :items="footerNavItems" />
+      <NavUser v-if="user" />
+      <NavMain v-else :items="loginSignin"> </NavMain>
+    </SidebarFooter>
+  </Sidebar>
+  <slot />
 </template>
