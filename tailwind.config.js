6d5aac47905d5abe45732aa6a494027bafa6e7e8
Galactic1106 <aleksander.sardara@gmail.com>
2026-02-07 18:21:57 +0100

    start of project version 3 with go+postgres for the api and svelte for frontend

--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: ['./resources/views/**/*.blade.php', './resources/js/**/*.vue', './resources/js/**/*.js'],
-    theme: { extend: {} },
-    plugins: [],
-};
