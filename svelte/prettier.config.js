79dcae60e1a24b2c79eba5fb09566a3a096912e7
Galactic1106 <aleksander.sardara@gmail.com>
2026-08-09 18:09:27 +0200

    changed tabwidth

--- svelte/prettier.config.js
+++ svelte/prettier.config.js
@@ -1,12 +1,13 @@
 /** @type {import("prettier").Config} */
 const config = {
-	useTabs: true,
-	singleQuote: true,
-	trailingComma: 'none',
-	printWidth: 100,
-	plugins: ['prettier-plugin-svelte', 'prettier-plugin-tailwindcss'],
-	overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }],
-	tailwindStylesheet: './src/routes/layout.css'
+  useTabs: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  plugins: ['prettier-plugin-svelte', 'prettier-plugin-tailwindcss'],
+  overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }],
+  tailwindStylesheet: './src/routes/layout.css',
+  tabWidth: 2,
 };
 
 export default config;
